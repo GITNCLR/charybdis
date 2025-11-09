f8ac04da482614e384475f38e8e6d90f23accb79
Noah Christian Le Roy <noahleroy@gmail.com>
2025-11-10 00:27:41 +0100

    mirror on / led test

--- keyboards/bastardkb/charybdis/4x6/keymaps/noah/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/noah/keymap.c
@@ -225,7 +225,7 @@ bool is_mouse_record_user(uint16_t keycode, keyrecord_t *record) {
  * ├────────────────────────┤                 ├────────────────────────┤
  *    3   4  11  12  19  23                     52  48  41  40  33  32
  * ╰────────────────────────╯                 ╰────────────────────────╯
- *                       26  27  28     53  54  XX
+ *                       26  27  28     53  54  XX Rightside Thumb have a mapping issue,
  *                           25  24     55  XX
  *                     ╰────────────╯ ╰────────────╯
  *
@@ -267,14 +267,16 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
         rgb_matrix_set_color(i, base_rgb.r, base_rgb.g, base_rgb.b);
     }
 
-    if (is_keyboard_left()) {
-        rgb_matrix_set_color(3, 255, 0, 0);
-    } else {
-        rgb_matrix_set_color(3, 0, 0, 255);
-    }
-    rgb_matrix_set_color(54, 255, 0, 0);
-    rgb_matrix_set_color(40, 255, 0, 0);
-    rgb_matrix_set_color(28, 0, 0, 255);
+    // Example: Different color on LED 3 depending on left/right half
+    //    if (is_keyboard_left()) {
+    //        rgb_matrix_set_color(3, 255, 0, 0);
+    //    } else {
+    //        rgb_matrix_set_color(3, 0, 0, 255);
+    //    }
+
+    rgb_matrix_set_color(58, 0, 0, 255);
+    rgb_matrix_set_color(57, 0, 0, 255);
+    rgb_matrix_set_color(56, 0, 0, 255);
 
     return true;
 }
